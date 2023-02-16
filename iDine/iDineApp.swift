aac0ed81e0dac8dbaead8bd0b1b70333113ecf68
Philip Keller <philipjoelkeller@gmail.com>
2023-02-15 21:49:37 -0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Philip Keller on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
