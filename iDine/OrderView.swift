24a7c8e4e817106132391ab879e47bde2938feb7
Philip Keller <philipjoelkeller@gmail.com>
2023-02-15 23:28:43 -0800

    TabView - Youtube 8/14

---
+++ iDine/OrderView.swift
@@ -0,0 +1,42 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Philip Keller on 2/15/23.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Place Order") {
+                        Text("Check out")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+            .environmentObject(Order())
+    }
+}
